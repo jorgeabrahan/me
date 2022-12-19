7ccce9a521af5d8eff2bcd7b470a79e3f9d91dc2
Jorge <jorge24abrahan@gmail.com>
2022-12-19 14:07:12 -0600

    Add JavaScript linter

--- js/navbar.js
+++ js/navbar.js
@@ -1,29 +1,28 @@
-const menu = document.getElementById("menu");
-const btnMenu = document.getElementById("btnMenu");
+const menu = document.getElementById('menu');
+const btnMenu = document.getElementById('btnMenu');
 const iconMenu = btnMenu.firstElementChild;
 
 const toggleIcon = (iconMenu) => {
-    const iconText = iconMenu.textContent.toLowerCase().trim();
-    const icons = {
-        close: "menu",
-        menu: "close",
-    };
-    iconMenu.textContent = icons[iconText];
+  const iconText = iconMenu.textContent.toLowerCase().trim();
+  const icons = {
+    close: 'menu',
+    menu: 'close',
+  };
+  iconMenu.textContent = icons[iconText];
 };
 
 const onMenuInteraction = () => {
-    menu.classList.toggle("toolbar__menu--translate");
-    toggleIcon(iconMenu);
+  menu.classList.toggle('toolbar__menu--translate');
+  toggleIcon(iconMenu);
 };
 
-btnMenu.addEventListener("click", () => {
-    onMenuInteraction();
+btnMenu.addEventListener('click', () => {
+  onMenuInteraction();
 });
 
-const menuLinks = document.querySelectorAll(".menu__link");
+const menuLinks = document.querySelectorAll('.menu__link');
 menuLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-        onMenuInteraction();
-    });
+  link.addEventListener('click', () => {
+    onMenuInteraction();
+  });
 });
-
