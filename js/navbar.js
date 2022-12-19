acb47e4ee45f9151f1958d78a1b5f605378b5a98
Jorge <jorge24abrahan@gmail.com>
2022-12-19 13:55:34 -0600

    Show/Hide navbar on mobile

---
+++ js/navbar.js
@@ -0,0 +1,21 @@
+const menu = document.getElementById("menu");
+const btnMenu = document.getElementById("btnMenu");
+const iconMenu = btnMenu.firstElementChild;
+
+const toggleIcon = (iconMenu) => {
+    const iconText = iconMenu.textContent.toLowerCase().trim();
+    const icons = {
+        close: "menu",
+        menu: "close",
+    };
+    iconMenu.textContent = icons[iconText];
+};
+
+const onMenuInteraction = () => {
+    menu.classList.toggle("toolbar__menu--translate");
+    toggleIcon(iconMenu);
+};
+
+btnMenu.addEventListener("click", () => {
+    onMenuInteraction();
+});
