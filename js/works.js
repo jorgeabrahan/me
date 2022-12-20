ebcd07770b5b3b37bb0bfe83bf8268aee8256543
Jorge <jorge24abrahan@gmail.com>
2022-12-20 10:23:58 -0600

    Load my works section dynamically

---
+++ js/works.js
@@ -0,0 +1,94 @@
+const myWorks = [
+  {
+    img: {
+      src: './img/projects/budget-calculator.png',
+      alt: 'Budget Calculator Website',
+    },
+    title: 'Budget Calculator',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    technologies: [
+      'html',
+      'css',
+      'JavaScript',
+      'Firebase',
+      'Authentication',
+    ],
+    links: {
+      liveVersion: 'https://budgtcalc.web.app/',
+      sourceCode: 'https://github.com/jorgeabrahan/budgtcalc',
+    },
+  },
+  {
+    img: {
+      src: './img/projects/clients-registry.png',
+      alt: 'Clients Registry Website',
+    },
+    title: 'Clients Registry',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    technologies: [
+      'html',
+      'css',
+      'JavaScript',
+      'TypeScript',
+      'Authentication',
+    ],
+    links: {
+      liveVersion: 'https://lives-tool.web.app/',
+      sourceCode: 'https://github.com/jorgeabrahan/regclients',
+    },
+  },
+  {
+    img: {
+      src: './img/projects/digital-watch.png',
+      alt: 'Digital Watch Website',
+    },
+    title: 'Digital Watch',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    technologies: [
+      'html',
+      'css',
+      'JavaScript',
+      'Firebase',
+      'ReactJS',
+    ],
+    links: {
+      liveVersion: 'https://watch-js.web.app/',
+      sourceCode: 'https://github.com/jorgeabrahan/watch',
+    },
+  },
+];
+
+const createWorkHTML = (work) => {
+  const workCnt = document.createElement('div');
+  workCnt.className = 'works__card';
+  workCnt.innerHTML = `
+    <figure class="card__image-container">
+        <img
+            class="card__image"
+            src="${work.img.src}"
+            alt="${work.img.alt}"
+            loading="lazy"
+        />
+    </figure>
+    <div class="card__content">
+        <h3 class="card__title">${work.title}</h3>
+        <ul class="card__technologies">
+            ${work.technologies.map((technology) => `<li>${technology}</li>`).join('')}
+        </ul>
+        <button class="button">See project</button>
+    </div>
+  `;
+  return workCnt;
+};
+
+const worksCnt = document.querySelector('.works__cards');
+const loadMyWorksSection = () => {
+  const worksElements = myWorks.map((work) => createWorkHTML(work));
+  const worksFragment = document.createDocumentFragment();
+  worksElements.forEach((workElement) => {
+    worksFragment.appendChild(workElement);
+  });
+  worksCnt.appendChild(worksFragment);
+};
+
+loadMyWorksSection();
