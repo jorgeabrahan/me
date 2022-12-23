59cb0b9662721770d88cb2dfea722633181551c6
dfsalam <dfsalam@gmail.com>
2022-12-23 15:39:36 -0500

    Bug changes inside menu.js

--- js/bug.js
+++ js/bug.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-  if (5 == '5') {
+  if (5 === '5') {
     console.log('This message should not show up since the number 5 is not the same thing as the string "5"');
   }
 };
